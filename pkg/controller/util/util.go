0cdceff724ce30708f0072a4aa8bf97b8f0f052a
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2024-01-04 01:23:46 +0200

    Fix append to empty slice
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>

--- pkg/controller/util/util.go
+++ pkg/controller/util/util.go
@@ -58,8 +58,7 @@ func Addresses(ctx context.Context, client client.Client) ([]string, error) {
 		return nil, err
 	}
 
-	addresses := make([]string, len(nodeList.Items))
-
+	addresses := []string{}
 	for _, node := range nodeList.Items {
 		addresses = append(addresses, nodeAddress(&node))
 	}

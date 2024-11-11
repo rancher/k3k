ed8491c17b9afa98cad56868762b82c806eb6806
Enrico Candino <enrico.candino@suse.com>
2024-11-11 12:11:56 +0100

    added clusterset tests

--- pkg/controller/clusterset/clusterset_suite_test.go
+++ pkg/controller/clusterset/clusterset_suite_test.go
@@ -4,99 +4,85 @@ import (
 	"context"
 	"path/filepath"
 	"testing"
-	"time"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 	"github.com/rancher/k3k/pkg/apis/k3k.io/v1alpha1"
 	"github.com/rancher/k3k/pkg/controller/clusterset"
 	"github.com/rancher/k3k/pkg/log"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"go.uber.org/zap"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 func TestController(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "Controller Suite")
+	RunSpecs(t, "ClusterSet Controller Suite")
 }
 
 var (
+	testEnv   *envtest.Environment
 	k8sClient client.Client
+	ctx       context.Context
+	cancel    context.CancelFunc
 )
 
 var _ = BeforeSuite(func() {
-	ctx, _ := context.WithCancel(context.TODO())
 
 	By("bootstrapping test environment")
-	testEnv := &envtest.Environment{
+	testEnv = &envtest.Environment{
 		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "..", "charts", "k3k", "crds")},
 		ErrorIfCRDPathMissing: true,
-		CRDInstallOptions: envtest.CRDInstallOptions{
-			MaxTime: 60 * time.Second,
-		},
 	}
 	cfg, err := testEnv.Start()
 	Expect(err).NotTo(HaveOccurred())
-	Expect(cfg).NotTo(BeNil())
 
-	scheme := runtime.NewScheme()
-	err = v1alpha1.AddToScheme(scheme)
-	Expect(err).NotTo(HaveOccurred())
-
+	scheme := buildScheme()
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme})
 	Expect(err).NotTo(HaveOccurred())
-	Expect(k8sClient).NotTo(BeNil())
 
-	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
-		Scheme: scheme,
-	})
-	Expect(err).ToNot(HaveOccurred())
+	mgr, err := ctrl.NewManager(cfg, ctrl.Options{Scheme: scheme})
+	Expect(err).NotTo(HaveOccurred())
 
-	err = clusterset.Add(ctx, mgr, "", log.New(true))
-	Expect(err).ToNot(HaveOccurred())
+	ctx, cancel = context.WithCancel(context.Background())
+	nopLogger := &log.Logger{SugaredLogger: zap.NewNop().Sugar()}
+
+	err = clusterset.Add(ctx, mgr, "", nopLogger)
+	Expect(err).NotTo(HaveOccurred())
 
 	go func() {
 		defer GinkgoRecover()
 		err = mgr.Start(ctx)
-		Expect(err).ToNot(HaveOccurred(), "failed to run manager")
+		Expect(err).NotTo(HaveOccurred(), "failed to run manager")
 	}()
 })
 
-var _ = Describe("MemcachedController", func() {
-	Context("testing memcache controller", func() {
+var _ = AfterSuite(func() {
+	cancel()
 
-		It("should create deployment", func() {
-			clusterSet := &v1alpha1.ClusterSet{
-				ObjectMeta: v1.ObjectMeta{
-					Name:      "foo",
-					Namespace: "default",
-				},
-				Spec: v1alpha1.ClusterSetSpec{
-					MaxLimits: nil,
-				},
-			}
-
-			err := k8sClient.Create(context.Background(), clusterSet)
-			Expect(err).To(Not(HaveOccurred()))
-
-			// look for network policies etc
-
-			// createdDeploy := &appsv1.Deployment{}
-			// deployKey := types.NamespacedName{Name: memcached.Name, Namespace: memcached.Namespace}
-			// Eventually(func() bool {
-			// 	err := k8sClient.Get(ctx, deployKey, createdDeploy)
-			// 	return err == nil
-			// }, time.Second*10, time.Millisecond*250).Should(BeTrue())
-		})
-	})
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).NotTo(HaveOccurred())
 })
 
-// var _ = AfterSuite(func() {
-// 	cancel()
-// 	By("tearing down the test environment")
-// 	err := testEnv.Stop()
-// 	Expect(err).NotTo(HaveOccurred())
-// })
+func buildScheme() *runtime.Scheme {
+	scheme := runtime.NewScheme()
+
+	err := corev1.AddToScheme(scheme)
+	Expect(err).NotTo(HaveOccurred())
+	err = appsv1.AddToScheme(scheme)
+	Expect(err).NotTo(HaveOccurred())
+	err = networkingv1.AddToScheme(scheme)
+	Expect(err).NotTo(HaveOccurred())
+	err = v1alpha1.AddToScheme(scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	return scheme
+}

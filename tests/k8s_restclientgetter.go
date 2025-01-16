88e87e45ba1395d403dff349e5d9520066ea1068
Enrico Candino <enrico.candino@gmail.com>
2025-01-16 11:32:51 +0100

    testcontainers
    add build script
    
    dropped namespace from chart
    
    upload logs
    
    removed old tests

---
+++ tests/k8s_restclientgetter.go
@@ -0,0 +1,55 @@
+package k3k_test
+
+import (
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/client-go/discovery"
+	memory "k8s.io/client-go/discovery/cached"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type RESTClientGetter struct {
+	clientconfig    clientcmd.ClientConfig
+	restConfig      *rest.Config
+	discoveryClient discovery.CachedDiscoveryInterface
+}
+
+func NewRESTClientGetter(kubeconfig []byte) (*RESTClientGetter, error) {
+	clientconfig, err := clientcmd.NewClientConfigFromBytes([]byte(kubeconfig))
+	if err != nil {
+		return nil, err
+	}
+
+	restConfig, err := clientconfig.ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	dc, err := discovery.NewDiscoveryClientForConfig(restConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &RESTClientGetter{
+		clientconfig:    clientconfig,
+		restConfig:      restConfig,
+		discoveryClient: memory.NewMemCacheClient(dc),
+	}, nil
+}
+
+func (r *RESTClientGetter) ToRESTConfig() (*rest.Config, error) {
+	return r.restConfig, nil
+}
+
+func (r *RESTClientGetter) ToDiscoveryClient() (discovery.CachedDiscoveryInterface, error) {
+	return r.discoveryClient, nil
+}
+
+func (r *RESTClientGetter) ToRESTMapper() (meta.RESTMapper, error) {
+	return restmapper.NewDeferredDiscoveryRESTMapper(r.discoveryClient), nil
+}
+
+func (r *RESTClientGetter) ToRawKubeConfigLoader() clientcmd.ClientConfig {
+	return r.clientconfig
+}
---
+++ tests/tests_suite_test.go
@@ -0,0 +1,157 @@
+package k3k_test
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"maps"
+	"os"
+	"path"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/k3s"
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/chart/loader"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func TestTests(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Tests Suite")
+}
+
+var (
+	k3sContainer *k3s.K3sContainer
+	k8s          *kubernetes.Clientset
+)
+
+var _ = BeforeSuite(func() {
+	var err error
+	ctx := context.Background()
+
+	k3sContainer, err = k3s.Run(ctx, "rancher/k3s:v1.27.1-k3s1")
+	Expect(err).To(Not(HaveOccurred()))
+
+	kubeconfig, err := k3sContainer.GetKubeConfig(context.Background())
+	Expect(err).To(Not(HaveOccurred()))
+
+	initKubernetesClient(kubeconfig)
+	installK3kChart(kubeconfig)
+})
+
+func initKubernetesClient(kubeconfig []byte) {
+	restcfg, err := clientcmd.RESTConfigFromKubeConfig(kubeconfig)
+	Expect(err).To(Not(HaveOccurred()))
+
+	k8s, err = kubernetes.NewForConfig(restcfg)
+	Expect(err).To(Not(HaveOccurred()))
+}
+
+func installK3kChart(kubeconfig []byte) {
+	pwd, err := os.Getwd()
+	Expect(err).To(Not(HaveOccurred()))
+
+	k3kChart, err := loader.Load(path.Join(pwd, "../charts/k3k"))
+	Expect(err).To(Not(HaveOccurred()))
+
+	actionConfig := new(action.Configuration)
+
+	restClientGetter, err := NewRESTClientGetter(kubeconfig)
+	Expect(err).To(Not(HaveOccurred()))
+
+	releaseName := "k3k"
+	releaseNamespace := "k3k-system"
+
+	err = actionConfig.Init(restClientGetter, releaseNamespace, os.Getenv("HELM_DRIVER"), func(format string, v ...interface{}) {
+		fmt.Fprintf(GinkgoWriter, "helm debug: "+format+"\n", v...)
+	})
+	Expect(err).To(Not(HaveOccurred()))
+
+	iCli := action.NewInstall(actionConfig)
+	iCli.ReleaseName = releaseName
+	iCli.Namespace = releaseNamespace
+	iCli.CreateNamespace = true
+	iCli.Timeout = time.Minute
+	iCli.Wait = true
+
+	imageMap, _ := k3kChart.Values["image"].(map[string]any)
+	maps.Copy(imageMap, map[string]any{
+		"repository": "rancher/k3k",
+		"tag":        "dev",
+		"pullPolicy": "IfNotPresent",
+	})
+
+	sharedAgentMap, _ := k3kChart.Values["sharedAgent"].(map[string]any)
+	sharedAgentImageMap, _ := sharedAgentMap["image"].(map[string]any)
+	maps.Copy(sharedAgentImageMap, map[string]any{
+		"repository": "rancher/k3k-kubelet",
+		"tag":        "dev",
+	})
+
+	err = k3sContainer.LoadImages(context.Background(), "rancher/k3k:dev", "rancher/k3k-kubelet:dev")
+	Expect(err).To(Not(HaveOccurred()))
+
+	release, err := iCli.Run(k3kChart, k3kChart.Values)
+	Expect(err).To(Not(HaveOccurred()))
+
+	fmt.Fprintf(GinkgoWriter, "Release %s installed in %s namespace\n", release.Name, release.Namespace)
+}
+
+var _ = AfterSuite(func() {
+	// dump k3s logs
+	readCloser, err := k3sContainer.Logs(context.Background())
+	Expect(err).To(Not(HaveOccurred()))
+
+	logs, err := io.ReadAll(readCloser)
+	Expect(err).To(Not(HaveOccurred()))
+
+	logfile := path.Join(os.TempDir(), "k3s.log")
+	err = os.WriteFile(logfile, logs, 0644)
+	Expect(err).To(Not(HaveOccurred()))
+
+	fmt.Fprintln(GinkgoWriter, "k3s logs written to: "+logfile)
+
+	testcontainers.CleanupContainer(GinkgoTB(), k3sContainer)
+})
+
+var _ = When("k3k is installed", func() {
+	It("has to be in a Ready state", func() {
+
+		// check that at least a Pod is in Ready state
+		Eventually(func() bool {
+			opts := v1.ListOptions{LabelSelector: "app.kubernetes.io/name=k3k"}
+			podList, err := k8s.CoreV1().Pods("k3k-system").List(context.Background(), opts)
+
+			Expect(err).To(Not(HaveOccurred()))
+			Expect(podList.Items).To(Not(BeEmpty()))
+
+			isReady := false
+
+		outer:
+			for _, pod := range podList.Items {
+				for _, condition := range pod.Status.Conditions {
+					if condition.Status != corev1.ConditionTrue {
+						continue
+					}
+
+					if condition.Type == corev1.PodReady {
+						isReady = true
+						break outer
+					}
+				}
+			}
+
+			return isReady
+		}).
+			WithTimeout(time.Second * 10).
+			WithPolling(time.Second).
+			Should(BeTrue())
+	})
+})

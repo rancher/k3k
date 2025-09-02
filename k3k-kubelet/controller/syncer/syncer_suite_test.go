515cd3507b48f44a3a8aa20b7e211433ee93f722
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2025-09-02 15:45:11 +0300

    enable unit tests
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>

--- k3k-kubelet/controller/syncer/syncer_suite_test.go
+++ k3k-kubelet/controller/syncer/syncer_suite_test.go
@@ -166,12 +166,12 @@ var _ = Describe("Kubelet Controller", func() {
 		cancel()
 	})
 
-	// Describe("PriorityClass Syncer", PriorityClassTests)
-	// Describe("ConfigMap Syncer", ConfigMapTests)
-	// Describe("Secret Syncer", SecretTests)
-	// Describe("Service Syncer", ServiceTests)
-	// Describe("Ingress Syncer", IngressTests)
-	// Describe("PersistentVolumeClaim Syncer", PVCTests)
+	Describe("PriorityClass Syncer", PriorityClassTests)
+	Describe("ConfigMap Syncer", ConfigMapTests)
+	Describe("Secret Syncer", SecretTests)
+	Describe("Service Syncer", ServiceTests)
+	Describe("Ingress Syncer", IngressTests)
+	Describe("PersistentVolumeClaim Syncer", PVCTests)
 	Describe("HostServiceStatus Syncer", HostServiceTests)
 })
 

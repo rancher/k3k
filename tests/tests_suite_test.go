d18215e7be300bad58641005d82b37aa03b8f696
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2025-09-05 12:37:27 +0300

    fxing tests
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>

--- tests/tests_suite_test.go
+++ tests/tests_suite_test.go
@@ -127,7 +127,8 @@ func installK3kChart(kubeconfig []byte) {
 	iCli.Timeout = time.Minute
 	iCli.Wait = true
 
-	imageMap, _ := k3kChart.Values["controller.image"].(map[string]any)
+	controllerMap, _ := k3kChart.Values["controller"].(map[string]any)
+	imageMap, _ := controllerMap["image"].(map[string]any)
 	maps.Copy(imageMap, map[string]any{
 		"repository": "rancher/k3k",
 		"tag":        "dev",

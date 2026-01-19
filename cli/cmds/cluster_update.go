52a5299e2d3af4e11e179bdac780467fdd8ba9f3
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2026-01-19 13:22:16 +0200

    Fix map copy
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>

--- cli/cmds/cluster_update.go
+++ cli/cmds/cluster_update.go
@@ -191,7 +191,7 @@ func confirmClusterUpdate(oldClusterDetails, clusterDetails string) bool {
 
 func mapCopyWithDefault[M1 ~map[K]V, M2 ~map[K]V, K comparable, V any](dst M1, src M2) {
 	if dst == nil {
-		maps.Copy(make(map[K]V), src)
+		dst = make(map[K]V)
 	}
 
 	maps.Copy(dst, src)

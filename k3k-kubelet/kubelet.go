0a16522e394d60cb5c99f3363f93526ed95638d3
Hussein Galal <hussein.galal.ahmed.11@gmail.com>
2026-06-03 16:04:42 +0300

    Remove pod mutating webhook refrences (#858)
    Signed-off-by: galal-hussein <hussein.galal.ahmed.11@gmail.com>

--- k3k-kubelet/kubelet.go
+++ k3k-kubelet/kubelet.go
@@ -33,7 +33,6 @@ import (
 	ctrlserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	"github.com/rancher/k3k/k3k-kubelet/controller/syncer"
-	k3kwebhook "github.com/rancher/k3k/k3k-kubelet/controller/webhook"
 	"github.com/rancher/k3k/k3k-kubelet/provider"
 	"github.com/rancher/k3k/pkg/apis/k3k.io/v1beta1"
 	"github.com/rancher/k3k/pkg/controller"
@@ -136,12 +135,6 @@ func newKubelet(ctx context.Context, c *config) (*kubelet, error) {
 	})
 	if err != nil {
 		return nil, errors.New("unable to create controller-runtime mgr for virtual cluster: " + err.Error())
-	}
-
-	logger.Info("removing pod mutating webhook")
-
-	if err := k3kwebhook.RemovePodMutatingWebhook(ctx, virtualMgr.GetClient(), hostClient, c.ClusterName, c.ClusterNamespace); err != nil {
-		return nil, errors.New("unable to remove pod mutating webhook for virtual cluster: " + err.Error())
 	}
 
 	controllerName := c.AgentHostname

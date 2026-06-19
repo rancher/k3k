7f484e62aabb67381d81e3fa45fc85ebbeae9e29
Enrico Candino <enrico.candino@suse.com>
2026-06-19 16:06:31 +0200

    fix lint

--- tests/framework/portforward/portforward.go
+++ tests/framework/portforward/portforward.go
@@ -14,12 +14,13 @@ import (
 	"strconv"
 	"time"
 
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/portforward"
 	"k8s.io/client-go/transport/spdy"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const readyTimeout = 30 * time.Second

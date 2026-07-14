78367b2ec2471e7ee3a7f7b5a81512223b62d426
Kevin McDermott <bigkevmcd@gmail.com>
2026-07-14 11:02:34 +0100

    Update k3k-kubelet/controller/syncer/events.go
    Co-authored-by: Enrico Candino <enrico.candino@gmail.com>

--- k3k-kubelet/controller/syncer/events.go
+++ k3k-kubelet/controller/syncer/events.go
@@ -63,6 +63,7 @@ func AddEventSyncer(ctx context.Context, virtMgr, hostMgr manager.Manager, clust
 // between the host and virtual clusters.
 func (s *EventSyncer) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
 	logger := ctrl.LoggerFrom(ctx).WithValues("cluster", s.ClusterName, "clusterNamespace", s.ClusterNamespace)
+	ctx = ctrl.LoggerInto(ctx, logger)
 
 	event := &corev1.Event{}
 	if err := s.HostClient.Get(ctx, req.NamespacedName, event); err != nil {
